90dfee938d2512c937e90a4f729d0565054fd5a1
Laspargus <maxime.speroni@gmail.com>
2019-03-11 17:10:04 +0100

    add gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,63 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
+
+
+
+//= require jquery/jquery.min
+
+//= require popper/popper.min
+
+//= require bootstrap/js/bootstrap.min
+
+
+
+
+//= require slidebar/slidebar
+//= require classie
+//= require boomerang.min
+
+
+//= require bootstrap-notify/bootstrap-growl.min
+//= require scrollpos-styler/scrollpos-styler
+
+
+//= require adaptive-backgrounds/adaptive-backgrounds
+//= require countdown/js/jquery.countdown.min
+//= require dropzone/dropzone.min
+//= require easy-pie-chart/jquery.easypiechart.min
+//= require fancybox/js/jquery.fancybox.min
+//= require flatpickr/flatpickr.min
+//= require flip/flip.min
+//= require footer-reveal/footer-reveal.min
+//= require gradientify/jquery.gradientify.min
+//= require headroom/headroom.min
+//= require headroom/jquery.headroom.min
+//= require input-mask/input-mask.min
+
+//= require milestone-counter/jquery.countTo
+//= require nouislider/js/nouislider.min
+//= require paraxify/paraxify.min
+//= require select2/js/select2.min
+//= require sticky-kit/sticky-kit.min
+//= require swiper/js/swiper.min
+//= require textarea-autosize/autosize.min
+//= require typeahead/typeahead.bundle.min
+//= require typed/typed.min
+//= require vide/vide.min
+//= require viewport-checker/viewportchecker.min
+//= require wow/wow.min
+
+
+//= require isotope/isotope.min
+//= require imagesloaded/imagesloaded.pkgd.min
+
+
+
+
+
+
