d85d9802c3e59e45176cdb217a544ec1e00bd82b
Clodiap <clodiapp@gmail.com>
2019-03-12 15:26:12 +0100

    [progress] added resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'products/index'
-  get 'products/show'
-  get 'products/create'
-  get 'products/destroy'
   devise_for :users
+  resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
