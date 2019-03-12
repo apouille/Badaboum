e4736a608a33bc7ed068a5a6f9f9a64ae12fc2d3
vdepetigny <vdepetigny@yahoo.fr>
2019-03-12 18:54:04 +0100

    [Done] Functionnal new product form with Active Storage (local)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
 	belongs_to :seller, class_name:'User'
 	belongs_to :category
-	has_many_attached :product_pictures
+	has_many_attached :pictures
 
-	validates :title, :description, :price, presence: { message: "Merci de remplir tous les champs obligatoires!" };
+  validates :title, :description, :price, presence: { message: "Merci de remplir tous les champs obligatoires!" };
   validates :description, length: { minimum: 20, message: "La description doit faire au minimum 20 caractères" };
   validates :price, numericality: { greater_than: 1,  message: "Le prix doit être supérieur à 1€" };
   end
