447de0892d8efe761a0af17adb9a063fe20f5438
Saroumoun <bastien.hiel@gmail.com>
2019-03-12 13:05:37 +0100

    [in progress] Added association with profile table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_171954) do
+ActiveRecord::Schema.define(version: 2019_03_12_110438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,21 @@ ActiveRecord::Schema.define(version: 2019_03_11_171954) do
     t.index ["seller_id"], name: "index_products_on_seller_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "street"
+    t.string "city"
+    t.string "zip_code"
+    t.string "country", default: "France"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +64,5 @@ ActiveRecord::Schema.define(version: 2019_03_11_171954) do
   end
 
   add_foreign_key "products", "categories"
+  add_foreign_key "profiles", "users"
 end
