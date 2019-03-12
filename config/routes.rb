7b036ab282aa7b76b6d176e2efce87c2bda3cba3
Clodiap <clodiapp@gmail.com>
2019-03-12 15:13:08 +0100

    [progress] products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'products/index'
+  get 'products/show'
+  get 'products/create'
+  get 'products/destroy'
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
