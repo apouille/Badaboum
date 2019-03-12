58e2101b3e4ca26341113ce25c14487366c2fc8f
Saroumoun <bastien.hiel@gmail.com>
2019-03-12 13:06:18 +0100

    [in progress] Added seed for profile table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-
 name_category = ["Soins bébé", "Chaises hautes & sièges auto", "Poussettes", "Mobilier enfant", "Jeux & Jouets", "Livres & Scolarité", "Sécurité", "Allaitement & Repas", "Vêtements"]
 
 name_email = ["bastien.hiel@yopmail.com", "claudia.partonneau@yopmail.com", "maxime.speroni@yopmail.com", "viviane.depetigny@yopmail.com", "melanie.nguon@yopmail.com"]
+
+first_names = ["Bastien", "Claudia", "Maxime", "Viviane", "Mélanie"]
+last_names = ["Hiel", "Partonneau", "Speroni", "Depetigny", "Nguon"]
 
 i = 0
 5.times do
@@ -26,4 +19,20 @@ name_category.each do |i|
   Category.create!(
     title: i
   )
+end
+
+j = 0
+5.times do
+	profile = Profile.create!(
+		first_name: first_names[j],
+		last_name: last_names[j],
+		phone_number: "06#{rand(10000000..99999999)}",
+		street: "#{rand(1..100)} Avenue Kléber",
+		city: "Paris",
+		zip_code: "75016",
+		country: "France", 
+		description: "Un super être humain qui est tout le temps positif à propos de tout!",
+		user: User.find(j+1)
+	)
+	j += 1
 end
