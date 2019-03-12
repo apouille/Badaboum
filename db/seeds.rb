8f88ffc6a024a5b9dd6b3cb3e7c2ad25e98f89d4
apouille <nguon.melanie@gmail.com>
2019-03-12 11:48:01 +0100

    [fix] seed email

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-5.times do
-	user = User.create!(
-		email: "mimi#{rand(1..1000)}@yopmail.com",
-		password: "test1234",
-	)
-end
 
 name_category = ["Soins bébé", "Chaises hautes & sièges auto", "Poussettes", "Mobilier enfant", "Jeux & Jouets", "Livres & Scolarité", "Sécurité", "Allaitement & Repas", "Vêtements"]
+
+name_email = ["bastien.hiel@yopmail.com", "claudia.partonneau@yopmail.com", "maxime.speroni@yopmail.com", "viviane.depetigny@yopmail.com", "melanie.nguon@yopmail.com"]
+
+i = 0
+5.times do
+	user = User.create!(
+		email: name_email[i],
+		password: "test1234",
+	)
+	i += 1
+end
+
 
 name_category.each do |i|
   Category.create!(
