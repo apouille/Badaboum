6a28a6d5745e9dc9b36b619fb35000d5c4251010
vdepetigny <vdepetigny@yahoo.fr>
2019-03-12 17:00:02 +0100

    [Progress] Functionnal new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,30 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+  	@product = Product.new
+  	@categories = Category.all
+  end
+
   def create
+  	@categories = Category.all
+  	@product = Product.new(title: params[:title],
+                       description: params[:description],
+                       price: params[:price],
+                       brand: params[:brand],
+                       color: params[:color],
+                       size: params[:size],
+                       seller_id: current_user.id,
+                       category_id: params[:category],
+                       )
+    #@product.product_picture.attach(params[:product_picture])
+
+    if @product.save
+      redirect_to root_path, 
+      flash[:success] = "Le produit est bien enregistré!"
+    else
+      render 'new'
+    end
   end
 
   def destroy
