3fe269bc436559bd56100ce094c226eb22e025f7
Laspargus <maxime.speroni@gmail.com>
2019-03-13 14:58:57 +0100

    add ajax to wishlist product

--- app/controllers/wishlist_products_controller.rb
+++ app/controllers/wishlist_products_controller.rb
@@ -18,10 +18,12 @@ class WishlistProductsController < ApplicationController
    if @wishlist_product.save
      respond_to do |format|
        flash[:notice] = "produit ajouté à votre babylist" 
+
        format.html {redirect_back fallback_location: products_path}
+     
        format.js 
      end
-   
+   end
 
   end
 
@@ -33,8 +35,19 @@ class WishlistProductsController < ApplicationController
 
  
   def destroy
+    @product = Product.find(params[:product])
     @wishlist_product = WishlistProduct.find(params[:id])
     @wishlist_product.destroy
+     respond_to do |format|
+       flash[:notice] = "produit retiré de votre babylist" 
+
+       format.html {redirect_back fallback_location: products_path}
+       
+       format.js 
+     end
+
+
+
   end
 
 
