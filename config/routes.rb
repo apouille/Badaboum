e3877c23c40566f26a103a28b1deb2064c396c89
Laspargus <maxime.speroni@gmail.com>
2019-03-13 11:45:28 +0100

    [progress] wishlist html ok - ajax need to be done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+
   get 'products/index'
   get 'products/show'
   get 'products/create'
   get 'products/new'
   get 'products/update'
   get 'products/destroy'
-  devise_for :users
+  devise_for :users 
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   get 'kitui', to: 'home#kitui'
 
   resources :products
-
+  
+  resources :wishlist_products
+  
 
 end
