274fb3aad90cd7fedb83da4378c2817aa051f1d2
Laspargus <maxime.speroni@gmail.com>
2019-03-13 12:01:46 +0100

    fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
 
-  get 'products/index'
-  get 'products/show'
-  get 'products/create'
-  get 'products/new'
-  get 'products/update'
-  get 'products/destroy'
-  devise_for :users 
-=======
+
   devise_for :users
->>>>>>> delivery
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -20,13 +12,13 @@ Rails.application.routes.draw do
   get 'profile', to: 'home#profile'
 
   resources :products
-<<<<<<< HEAD
+
   
   resources :wishlist_products
   
-=======
+
   resource :profile, only: [:show, :edit, :update]
 
->>>>>>> delivery
+
 
 end
