13698b39ccd91379a4b2171d53405e26150ca586
apouille <nguon.melanie@gmail.com>
2019-03-15 14:58:41 +0100

    [in progress] mailer comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,30 @@
 class CommentsController < ApplicationController
 
-def new
-	@comment = Comment.new
-end
+after_action :notification_comment, only: [:create]
 
-def create
-	@comment = Comment.new(content: params[:content], user: current_user, product_id: params[:product_id])
-	@user = current_user
-
-	if @comment.save
-		flash[:notice] = "Votre commentaire a été ajouté avec succès"
-		SellerMailer.notification_comment(@seller_email).deliver_now
- 		redirect_to product_path(params[:product_id])
-    else
-      render :root_path
+	def new
+		@comment = Comment.new
 	end
-end
+
+	def create
+		@comment = Comment.new(content: params[:content], user: current_user, product_id: params[:product_id])
+		@user = current_user
+
+		if @comment.save
+			flash[:notice] = "Votre commentaire a été ajouté avec succès"
+	 		redirect_to product_path(params[:product_id])
+	    else
+	      render :root_path
+		end
+	end
+
+	def notification_comment
+		puts "*"*30
+		puts params
+		puts "*"*30
+		@product = Product.find(params[:product_id])
+		SellerMailer.notification_comment(current_user, @product).deliver_now
+	end
 
 end
+
