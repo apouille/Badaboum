a14993d63faeb0ce63b2359ccdf2fa9072ee496d
Saroumoun <bastien.hiel@gmail.com>
2019-03-15 18:12:55 +0100

    [progress]Changed validator birthday_cannot_be_in_the_futur

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,11 +1,12 @@
 class Child < ApplicationRecord
 	belongs_to :profile
 	enum sex: { girl:1, boy:2 }
-	validates :nickname, :birthday, :sex , presence: { message: "Merci de remplir tous les champs du formulaire!" }
+	validates :nickname, :birthday, :sex, presence: { message: "Merci de remplir tous les champs du formulaire!" }
 
 	validate :birthday_cannot_be_in_the_futur
 
   def birthday_cannot_be_in_the_futur
-    errors.add(:birthday, "ne peut pas être dans le futur") if birthday > Date.today
-  end  
+    errors.add(:birthday, "La date de naissance ne peut pas être dans le passé") if
+      !birthday.blank? and birthday > Date.today
+  end
 end
