b6864dea391f9ac5e64c2af4bd011b5834f73e6d
Saroumoun <bastien.hiel@gmail.com>
2019-03-15 12:04:46 +0100

    [progress]Added column icon to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_145643) do
+ActiveRecord::Schema.define(version: 2019_03_15_092328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_145643) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "children", force: :cascade do |t|
