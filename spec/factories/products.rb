5255e4ba3d74b79448bcf67ed903b5375732162d
Saroumoun <bastien.hiel@gmail.com>
2019-03-16 17:02:18 +0100

    [progress]Added associations tests for product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-  	title { Faker::Lorem.words(1) }
+  	title { Faker::Lorem.words }
     price { rand(1..1000) }
     description { Faker::Lorem.paragraph(2) }
     brand { Faker::Company.name }
