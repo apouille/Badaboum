21e7fe3c48727a08cb9d54161610104cefca8335
Saroumoun <bastien.hiel@gmail.com>
2019-03-16 16:38:53 +0100

    [progress]Added validation for status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
-	enum status: { in_progress:1, paid_with_stripe:2 }
 	belongs_to :user
 	belongs_to :product
+
+	enum status: { in_progress:1, paid_with_stripe:2 }
+	validates :status, presence:true
 end
