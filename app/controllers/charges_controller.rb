da387db305fa05356eef2882f9497ec8bd616275
Laspargus <maxime.speroni@gmail.com>
2019-03-18 18:37:01 +0100

    [fix] - translate stripe checkout and button

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,7 +10,6 @@ class ChargesController < ApplicationController
   	@order = Order.where(user_id: current_user.id, status: 1).last
   	@product =@order.product
   	@amount = @order.product.price*100
-  	#@amount_for_seller=(@amount*0.95).to_i
   	@application_fee_amount = (@amount*0.1).to_i
   	@seller_uid = @order.product.seller.stripe_uid
 
@@ -18,7 +17,7 @@ class ChargesController < ApplicationController
       email: params[:stripeEmail],
       source: params[:stripeToken],
     )
- 		charge = Stripe::Charge.create({ customer: customer.id, amount: @amount, application_fee_amount: @application_fee_amount, description: 'Rails Stripe customer', currency: 'eur', transfer_data: {destination: @seller_uid}
+ 		charge = Stripe::Charge.create({ currency: 'eur', customer: customer.id, amount: @amount, application_fee_amount: @application_fee_amount, description: 'Rails Stripe customer', currency: 'eur', transfer_data: {destination: @seller_uid}
 })
 
       @order.update(stripe_customer_id: charge[:customer], status: 2)
@@ -33,13 +32,5 @@ class ChargesController < ApplicationController
       redirect_to new_charge_path
   	end
 
-#  def confirmation_send
-#    @items = current_user.carts.last.cart_items
-#    UserMailer.confirmation_email(current_user, @items).deliver_now
-#  end
-#
-#  def new_order_send
-#    @cart_items = current_user.carts.last.cart_items
-#    AdminMailer.new_order_email(current_user, @cart_items).deliver_now
-#  end
+
 end
