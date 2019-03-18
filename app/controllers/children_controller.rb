acd09c9e76ae501483fe4f16524187486746c939
Laspargus <maxime.speroni@gmail.com>
2019-03-18 19:15:41 +0100

    [fix] _ redirection and message  after children form edit

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -8,13 +8,15 @@ class ChildrenController < ApplicationController
     @profile = current_user.profile
 
     params[:sex] == "Fille" ? sex = 1 : sex = 2
-		
-		@child = Child.new(nickname: params[:nickname], sex: sex, birthday: params[:birthday], profile: @profile)
+	@nickname = params[:nickname]
+	@child = Child.new(nickname: params[:nickname], sex: sex, birthday: params[:birthday], profile: @profile)
 		if @child.save
-      redirect_to root_path
-      flash[:success] = "Votre enfant est bien enregistré!"
-    else
-      redirect_back fallback_location: root_path
-    end
+			flash[:success] = "#{@nickname} a bien été ajouté"
+	    	redirect_to profile_path
+	      
+	    else
+	    	flash[:error] = "Veuillez remplir tous les champs"
+	    	redirect_back fallback_location: edit_profile_path
+	    end
 	end
 end
