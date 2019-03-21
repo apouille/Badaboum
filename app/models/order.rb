b0730de6814af02646eccf11d5693c660f884d0a
Saroumoun <bastien.hiel@gmail.com>
2019-03-21 18:39:36 +0100

    [done]Added validation on notation

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@ class Order < ApplicationRecord
 
 	enum status: { in_progress:1, paid_with_stripe:2, completed:3, disputed:4, refunded:5 }
 	validates :status, presence:true
+  validates :notation, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
