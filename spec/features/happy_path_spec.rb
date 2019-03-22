df2049f0f36e9a8401e37c1fa6a99aadcc1156c3
Saroumoun <bastien.hiel@gmail.com>
2019-03-22 16:29:52 +0100

    [progress]Started testing happy path of badaboum

--- spec/features/happy_path_spec.rb
+++ spec/features/happy_path_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature "the buying process" do
   background do
     @user = FactoryBot.create(:user)
+    @product = FactoryBot.create(:product)
   end
 
   scenario "user sign in then buy a product" do
@@ -16,12 +17,13 @@ feature "the buying process" do
 
     click_button 'Se connecter'
 
-    click_link_or_button("Je découvre")  
+    click_link_or_button("Je découvre")
 
-
+    page.find(:xpath, '//*[@id="products_grid"]/div[1]/div/div[1]/div/a').click
     
+    click_link_or_button("Acheter ce produit")  
 
-
+    expect(page).to have_content("Méthode de paiement")
   end
 end
 
